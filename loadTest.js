99e9cdd8fffd14bbf11329036176fd31aeea9c03
Malcolm Lashley <mlashley@gmail.com>
2020-08-24 09:40:16 +0100

    Update CLI example to show how to use per-test callbacks for logging

--- loadTest.js
+++ loadTest.js
@@ -4,13 +4,23 @@
 var path = require('path'), 
     async = require('async'), 
     newman = require('newman'),
+    util = require('util')
     // options for the parallel collection runs
     options = {
-        collection: path.join(__dirname, './ExampleEchoRequestMethodsCollection.json')
+        collection: path.join(__dirname, './ExampleEchoRequestMethodsCollection.json'),
+	reporters: 'cli'
     },
     // callback function that marks the end of the current collection run, when called
     parallelCollectionRun = function (done) {
-        newman.run(options, done);
+        newman.run(options, done)
+	// See https://www.npmjs.com/package/newman#api-reference for options
+	.on('request', function(err,summary) {
+          // Example of how to dump the object if the Newman API docs are lacking
+          // console.log('E: ' + err + " S: " + util.inspect(summary,{showHidden: false, depth: null}));
+	  
+          // Example of how to print e.g. a single item from the callback object
+          // console.log('E: ' + err + " S: " + summary.response.code)
+	});
     };
 
 // Run the Postman sample collection thrice, in parallel.
